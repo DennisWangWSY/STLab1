2e9ca62460dfa66583a27b1f6b8fe6353d8551c4
Dennis <sywang@tju.edu.cn>
2016-03-18 15:24:09 +0800

    remove unused imports

--- JUnitTest/com/Triangle/IsEquilateralTest.java
+++ JUnitTest/com/Triangle/IsEquilateralTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collection;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
